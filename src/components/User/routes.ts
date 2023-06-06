eeb1507ca6306a4daf70ab19ca213d6936b58efc
Bobby Melmeth <bobby@moonward.com.au.com>
2023-06-07 09:38:43 +1000

    remove arbitrary comments

--- src/components/User/routes.ts
+++ src/components/User/routes.ts
@@ -3,16 +3,16 @@ import * as controller from './controller';
 
 const router = express.Router();
 
-// Create a new user
+
 router.post('/', controller.createUser);
 
-// Get user by ID
+
 router.get('/:id', controller.getUserById);
 
-// Update user
+
 router.put('/:id', controller.updateUser);
 
-// Delete user
+
 router.delete('/:id', controller.deleteUser);
 
 export default router;
